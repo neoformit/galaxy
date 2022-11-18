2acef4659860469b4f1859c29ce296225c095f7c
Ahmed Awan <newadmin@galaxyc.local>
2022-11-17 20:57:27 -0500

    External Login Flow: Redirect users if account already exists

--- client/src/components/login/LoginForm.vue
+++ client/src/components/login/LoginForm.vue
@@ -3,14 +3,38 @@
         <div class="row justify-content-md-center">
             <template v-if="!confirmURL">
                 <div class="col col-lg-6">
-                    <b-alert :show="messageShow" :variant="messageVariant" v-html="messageText" />
-                    <b-form id="login" @submit.prevent="submitGalaxyLogin()">
-                        <b-card no-body header="Welcome to Galaxy, please log in">
+                    <b-alert :show="!!messageText" :variant="messageVariant">
+                        {{ messageText }}
+                    </b-alert>
+                    <b-alert :show="!!connectExternalURL" variant="info">
+                        Reminder: Registration and usage of multiple accounts is tracked and such accounts are subject
+                        to termination and data deletion. Connect existing account now to avoid possible loss of data.
+                    </b-alert>
+                    <b-form id="login" @submit.prevent="submitLogin()">
+                        <b-card no-body>
+                            <b-card-header>
+                                <span v-if="!connectExternalURL"> Welcome to Galaxy, please log in </span>
+                                <span v-else>
+                                    There already exists a user with the email <i>{{ connectExternalURL }}</i
+                                    >. To associate this external login, you must first be logged in as that existing
+                                    account.
+                                </span>
+                            </b-card-header>
                             <b-card-body>
                                 <div>
                                     <!-- standard internal galaxy login -->
                                     <b-form-group label="Public Name or Email Address">
-                                        <b-form-input v-model="login" name="login" type="text" />
+                                        <b-form-input
+                                            v-if="!connectExternalURL"
+                                            v-model="login"
+                                            name="login"
+                                            type="text" />
+                                        <b-form-input
+                                            v-else
+                                            disabled
+                                            :value="connectExternalURL"
+                                            name="login"
+                                            type="text" />
                                     </b-form-group>
                                     <b-form-group label="Password">
                                         <b-form-input v-model="password" name="password" type="password" />
@@ -29,37 +53,31 @@
                                 </div>
                             </b-card-body>
                             <b-card-footer>
-                                Don't have an account?
-                                <span v-if="allowUserCreation">
-                                    <a
-                                        id="register-toggle"
-                                        href="javascript:void(0)"
-                                        role="button"
-                                        @click.prevent="toggleLogin"
-                                        >Register here.</a
-                                    >
+                                <span v-if="!connectExternalURL">
+                                    Don't have an account?
+                                    <span v-if="allowUserCreation">
+                                        <a
+                                            id="register-toggle"
+                                            href="javascript:void(0)"
+                                            role="button"
+                                            @click.prevent="toggleLogin">
+                                            Register here.
+                                        </a>
+                                    </span>
+                                    <span v-else>
+                                        Registration for this Galaxy instance is disabled. Please contact an
+                                        administrator for assistance.
+                                    </span>
                                 </span>
                                 <span v-else>
-                                    Registration for this Galaxy instance is disabled. Please contact an administrator
-                                    for assistance.
+                                    Do not wish to connect to an external provider?
+                                    <a href="javascript:void(0)" role="button" @click.prevent="returnToLogin"
+                                        >Return to login here.</a
+                                    >
                                 </span>
                             </b-card-footer>
                         </b-card>
                     </b-form>
-
-                    <b-modal id="duplicateEmail" ref="duplicateEmail" centered title="Duplicate Email" ok-only>
-                        <p>
-                            There already exists a user with this email. To associate this external login, you must
-                            first be logged in as that existing account.
-                        </p>
-
-                        <p>
-                            Reminder: Registration and usage of multiple accounts is tracked and such accounts are
-                            subject to termination and data deletion. Connect existing account now to avoid possible
-                            loss of data.
-                        </p>
-                        -->
-                    </b-modal>
                 </div>
             </template>
 
@@ -122,6 +140,13 @@ export default {
             var urlParams = new URLSearchParams(window.location.search);
             return urlParams.has("confirm") && urlParams.get("confirm") == "true";
         },
+        connectExternalURL() {
+            var urlParams = new URLSearchParams(window.location.search);
+            return urlParams.get("connect_external");
+        },
+        welcomeUrlWithRoot() {
+            return safePath(this.welcomeUrl);
+        },
     },
     methods: {
         toggleLogin() {
@@ -129,7 +154,11 @@ export default {
                 this.$root.toggleLogin();
             }
         },
-        submitGalaxyLogin(method) {
+        submitLogin() {
+            let redirect = this.redirect;
+            if (this.connectExternalURL) {
+                this.login = this.connectExternalURL;
+            }
             if (localStorage.getItem("redirect_url")) {
                 this.redirect = localStorage.getItem("redirect_url");
             }
@@ -140,10 +169,12 @@ export default {
                     if (response.data.message && response.data.status) {
                         alert(response.data.message);
                     }
-                    if (response.data.expired_user) {
-                        window.location = `${rootUrl}root/login?expired_user=${response.data.expired_user}`;
-                    } else if (response.data.redirect) {
-                        window.location = encodeURI(response.data.redirect);
+                    if (data.expired_user) {
+                        window.location = safePath(`/root/login?expired_user=${data.expired_user}`);
+                    } else if (data.redirect) {
+                        window.location = encodeURI(data.redirect);
+                    } else if (this.connectExternalURL) {
+                        window.location = safePath("/user/external_ids?connect_external=true");
                     } else {
                         window.location = `${rootUrl}`;
                     }
@@ -151,7 +182,12 @@ export default {
                 .catch((error) => {
                     this.messageVariant = "danger";
                     const message = error.response.data && error.response.data.err_msg;
-                    this.messageText = message || "Login failed for an unknown reason.";
+                    if (this.connectExternalURL && message && message.toLowerCase().includes("invalid")) {
+                        this.messageText =
+                            message + " Try logging in to the existing account through an external provider below.";
+                    } else {
+                        this.messageText = message || "Login failed for an unknown reason.";
+                    }
                 });
         },
         setRedirect(url) {
@@ -171,6 +207,9 @@ export default {
                     const message = error.response.data && error.response.data.err_msg;
                     this.messageText = message || "Password reset failed for an unknown reason.";
                 });
+        },
+        returnToLogin() {
+            window.location = safePath("/login/start");
         },
     },
 };
